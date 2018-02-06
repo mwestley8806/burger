04fb4177e5b4e591173c2c70d8b7a23c46c051ed
Michael Westley <mwestley8806@yahoo.com>
2018-02-05 21:58:21 -0500

    update 1

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bob', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Carole', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Michael', true);
